63dbd747d609e387b35d4f1fc162364eb42e557d
Puthikunkim <jerryputhikunkim@gmail.com>
2024-05-17 17:42:26 +1200

    Haven't attempted any test cases yet just implementing the fundamental logic

--- src/main/java/nz/ac/auckland/se281/Country.java
+++ src/main/java/nz/ac/auckland/se281/Country.java
@@ -3,10 +3,12 @@ package nz.ac.auckland.se281;
 public class Country {
   private String name;
   private String continent;
+  private int tax;
 
-  public Country(String name, String continent) {
+  public Country(String name, String continent, int tax) {
     this.name = name;
     this.continent = continent;
+    this.tax = tax;
   }
 
   public String getName() {
@@ -15,5 +17,31 @@ public class Country {
 
   public String getContinent() {
     return continent;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    result = prime * result + ((continent == null) ? 0 : continent.hashCode());
+    result = prime * result + tax;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    Country other = (Country) obj;
+    if (name == null) {
+      if (other.name != null) return false;
+    } else if (!name.equals(other.name)) return false;
+    if (continent == null) {
+      if (other.continent != null) return false;
+    } else if (!continent.equals(other.continent)) return false;
+    if (tax != other.tax) return false;
+    return true;
   }
 }
--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -1,5 +1,6 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +18,16 @@ public class MapEngine {
   private void loadMap() {
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
+  }
+
+  public void addCountry(Country country) {
+    adjCountries.putIfAbsent(
+        country, new ArrayList<>()); // DO I NEED PUTIFABSENT? ##############???????????????????????
+  }
+
+  public void addBorder(Country country1, Country country2) {
+    adjCountries.get(country1).add(country2);
+    adjCountries.get(country2).add(country1);
   }
 
   /** this method is invoked when the user run the command info-country. */
