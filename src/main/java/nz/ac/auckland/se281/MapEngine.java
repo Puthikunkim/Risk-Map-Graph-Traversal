7d99fe04243c3833627a94c4ed55962a0f71071c
Puthikunkim <jerryputhikunkim@gmail.com>
2024-05-17 17:29:17 +1200

    Making start on assignment 3

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -1,12 +1,15 @@
 package nz.ac.auckland.se281;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /** This class is the main entry point. */
 public class MapEngine {
+  private Map<Country, List<Country>> adjCountries;
 
   public MapEngine() {
-    // add other code here if you want
+    this.adjCountries = new HashMap<>();
     loadMap(); // keep this mehtod invocation
   }
 
@@ -14,7 +17,6 @@ public class MapEngine {
   private void loadMap() {
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
-    // add code here to create your data structures
   }
 
   /** this method is invoked when the user run the command info-country. */
